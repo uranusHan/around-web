a3ed7342bf9934eeaad73bc81b188c43cfc581c0
uranusHan <uranushan2017@gmail.com>
2017-12-12 13:30:24 -0800

    around-web-stage2-addloginform

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Register} from "./Register";
+import {Login} from "./Login";
 
 export class Main extends React.Component {
     render() {
         return (
             <section className="main">
-                <Register />
+                <Login />
             </section>
         );
     }
