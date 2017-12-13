3ffb2f8d904998ead22ce40253fb71d3cc517d7c
uranusHan <uranushan2017@gmail.com>
2017-12-13 15:47:35 -0800

    add-post

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import $ from 'jquery';
 import { Tabs, Button, Spin } from 'antd';
-import {GEO_OPTIONS, POS_KEY} from "../constants";
+import {API_ROOT, GEO_OPTIONS, POS_KEY, AUTH_PREFIX, TOKEN_KEY} from "../constants";
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
     state = {
+        posts:[],
         error: '',
         loadingGeoLocation: false,
     }
@@ -32,6 +34,7 @@ export class Home extends React.Component {
         //destructor
         const {latitude: lat, longitude: lon} = position.coords;
         localStorage.setItem(POS_KEY, JSON.stringify({lat: lat, lon: lon}));
+        this.loadNearbyPosts();
     }
     onFailedLoadGeoLocation = (error) => {
         this.setState({loadingGeoLocation: false, error: 'Failed to load geo location!'});
@@ -45,6 +48,32 @@ export class Home extends React.Component {
             return <Spin tip="Loading geo location ..." />
         }
         return null;
+    }
+    loadNearbyPosts = () => {
+        const {lat, lon} = JSON.parse(localStorage.getItem(POS_KEY));
+        $.ajax({
+            url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`,
+            method: 'GET',
+            header: {
+                Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`
+            },
+        })
+            .then((response) => {
+                console.log(response);
+                this.setState({
+                    posts: response
+                });
+            }, (error) => {
+                this.setState({
+                    error: error.responseText
+                });
+            })
+            .catch((error) => {
+                this.setState({
+                    error: error
+                });
+            });
+
     }
     render() {
         return (
